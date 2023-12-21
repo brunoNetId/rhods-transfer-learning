e3ea800a5c15868464ab13bcea9748a369c1614b
brunoNetId <bruno.meseguer@gmail.com>
2023-12-21 18:05:27 +0100

    added additional client samples

--- client/infer.sh
+++ client/infer.sh
@@ -1,11 +1,17 @@
 # set -x
 
-image=./lemon.jpg
+# image=./lemon.jpg
+# image=./earl-grey.jpg
+# image=./earl-grey2.jpeg
+# image=./earl-grey4.jpeg
+# image=./kid.jpg
+image=./apple.jpg
+server=https://tf-server-ai-demo.apps.cluster-2lngl.sandbox1314.opentlc.com
 
 curl -X POST \
 -v \
 -H "content-type: application/json" \
-"https://tf-server-ai-demo.apps.cluster-rhhz8.dynamic.redhatworkshops.io/v1/models/tea_model_b64:predict" \
+$server"/v1/models/tea_model_b64:predict" \
 -d '
 {
    "instances":
