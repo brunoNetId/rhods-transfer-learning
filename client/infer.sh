2cd3cfec6cc33e3cb1856e80e6317b72ecd13f9b
Stelio Kousouris <stelios.kousouris>
2024-02-05 15:44:50 +0000

    infer.sh dynamic hostname capture and correction of curl request

--- client/infer.sh
+++ client/infer.sh
@@ -2,7 +2,7 @@
 
 #server=https://tf-server-edge1.apps.cluster-252hm.sandbox1471.opentlc.com
 
-#server=https://$(oc get route tf-server -o jsonpath='{.spec.host}')
+server=https://$(oc get route tf-server -o jsonpath='{.spec.host}')
 
 echo
 echo $server
@@ -18,12 +18,7 @@ image=./green.jpg
 # image=./kid.jpg
 # image=./apple.jpg
 
-echo "---------------------------------"
-echo "curl -X POST -v -H \"content-type: application/json\" ${server}\"/v1/models/tea_model_b64:predict\" -d ' { \"instances\": [ {\"b64\": \"'$(base64 -i $image)'\"} ]}'"
-echo "---------------------------------"
-echo
-echo
-echo
+#curl -X POST -v -H "content-type: application/json" $server"/v1/models/tea_model_b64:predict" -d ' { "instances": [ { "b64": "'$(base64 -w 0 -i $image)'" } ] }'
 
 curl -X POST \
 -v \
@@ -34,7 +29,7 @@ $server"/v1/models/tea_model_b64:predict" \
    "instances":
    [
 	{
-	   "b64": "'$(base64 -i $image)'"
+	   "b64": "'$(base64 -w 0 -i $image)'"
 	}
    ]
 }'
