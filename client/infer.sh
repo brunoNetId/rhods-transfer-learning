cff80cc6eea69119477a0df6c9b42130359fcaaa
Stelio Kousouris <stelios.kousouris>
2024-02-05 15:32:21 +0000

    infer.sh dynamic hostname capture

--- client/infer.sh
+++ client/infer.sh
@@ -1,6 +1,13 @@
 # set -x
 
-server=https://tf-server-edge1.apps.cluster-252hm.sandbox1471.opentlc.com
+#server=https://tf-server-edge1.apps.cluster-252hm.sandbox1471.opentlc.com
+
+#server=https://$(oc get route tf-server -o jsonpath='{.spec.host}')
+
+echo
+echo $server
+echo
+sleep 2s
 
 # image=./lemon.jpg
 image=./green.jpg
@@ -10,6 +17,13 @@ image=./green.jpg
 # image=./earl-grey4.jpeg
 # image=./kid.jpg
 # image=./apple.jpg
+
+echo "---------------------------------"
+echo "curl -X POST -v -H \"content-type: application/json\" ${server}\"/v1/models/tea_model_b64:predict\" -d ' { \"instances\": [ {\"b64\": \"'$(base64 -i $image)'\"} ]}'"
+echo "---------------------------------"
+echo
+echo
+echo
 
 curl -X POST \
 -v \
